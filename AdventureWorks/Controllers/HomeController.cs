d0d501f38dc68946a753b7cd6edebab8dd8345e5
Throughfall <throughfall@googlemail.com>
2017-06-06 10:39:06 +0200

    About page content revised

--- AdventureWorks/Controllers/HomeController.cs
+++ AdventureWorks/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace AdventureWorks.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Beispielanwendung zur Adventure Works Cycles Datenbank.";
 
             return View();
         }
